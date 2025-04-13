c0c022ff07a4af370062526253317ae8dadcca11
Pedro Elizalde Acevedo <68881110+PedroElizalde01@users.noreply.github.com>
2025-04-12 22:53:39 -0300

    Update GameManager.cs

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,14 +8,14 @@ public class GameManager : MonoBehaviour
     public AudioClip loseSound;
     public UIManager uiManager;
     public DataCollector dataCollector;
-public int roundsPerTrial = 20; // default
+public int roundsPerTrial = 20;
 
-public GameObject startGamePanel; // para mostrar "Presione Espacio"
+public GameObject startGamePanel;
 
 private bool waitingToStart = false;
 
     private int currentTrial = 0;
-    private int[] trialOrder = new int[] { 0, 1, 2 }; // puede cambiar a [0,2,1] para versión B
+    private int[] trialOrder = new int[] { 0, 1, 2 };
 
     void Start()
     {
