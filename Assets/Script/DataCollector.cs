71637c6baa32c503a168f345a420d90e5faf9b38
Elian Embon <106391341+elianembon@users.noreply.github.com>
2025-08-06 20:28:05 -0300

    Fixes

--- Assets/Script/DataCollector.cs
+++ Assets/Script/DataCollector.cs
@@ -100,7 +100,7 @@ public class DataCollector : MonoBehaviour
         {
             List<string> lines = new List<string>();
 
-            lines.Add("Trial,Correct,Total,A,B,C");
+            lines.Add("Trial,Correct,Total,A,B,C,D");
             lines.Add($"{trials[0].trial},{trials[0].correct},{trials[0].total},{trials[0].a},{trials[0].b},{trials[0].c}, {trials[0].d}");
             lines.Add($"{trials[1].trial},{trials[1].correct},{trials[1].total},{trials[1].a},{trials[1].b},{trials[1].c},{trials[1].d}");
             lines.Add($"{trials[2].trial},{trials[2].correct},{trials[2].total},{trials[2].a},{trials[2].b},{trials[2].c}, {trials[2].d}");
